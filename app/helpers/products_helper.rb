4ab9aa5f796d242660a6a6d60ac8b24afa0c494e
wdcunha <wdcunha@gmail.com>
2018-09-28 20:14:03 +0100

    Basic prodcut listing and price field

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  def print_price(price)
+    # "£ #{price}"
+    # format("£ %.2f", price)
+    number_to_currency price
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates_numericality_of :price
+
+  def price=(input)
+    input.delete!("$")
+    super
+  end  
 end
