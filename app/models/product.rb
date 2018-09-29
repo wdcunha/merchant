02adeae8f26339457ec2b401e696524226ab5af8
wdcunha <wdcunha@gmail.com>
2018-09-29 14:46:56 +0100

    implemented stock and appearence of pages with css file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,10 @@
 class Product < ApplicationRecord
   validates_numericality_of :price
 
+  validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def price=(input)
     input.delete!("$")
     super
-  end  
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_152029) do
+ActiveRecord::Schema.define(version: 2018_09_28_191619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_152029) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stock", default: 0
   end
 
 end
